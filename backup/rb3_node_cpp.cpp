ffb3adfe5d9694099a9b24360316ad4e7adde9fa
Mads Thede <madskt2002@gmail.com>
2023-11-02 20:47:04 +0100

    flyttet publisher til main

--- backup/rb3_node_cpp.cpp
+++ backup/rb3_node_cpp.cpp
@@ -13,8 +13,8 @@
 #include "portaudio.h"
 
 #define SAMPLE_RATE 8000
-#define RECORDING_DURATION_SECONDS 0.2    //resolution = (sample_rate /(sample_rate*duration))
-#define FRAMES_PER_BUFFER  1600
+#define RECORDING_DURATION_SECONDS 0.2 // resolution = (sample_rate /(sample_rate*duration))
+#define FRAMES_PER_BUFFER 1600
 #define NUM_CHANNELS 1
 
 using namespace std;
@@ -28,13 +28,13 @@ public:
     // Create publisher for publishing veloctiy commands to topic "cmd_vel"
     RB3_cpp_publisher()
         : Node("rb3_cpp_publisher")
-    {   
+    {
         publisher_ = this->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
     }
 
     // Class function to be called when you want to publish velocity commands
     void publish_vel(float lin_vel_cmd, float ang_vel_cmd)
-    {   
+    {
         // Set angular velocity to desired value (ie. turning)
         msg.angular.x = 0;
         msg.angular.y = 0;
@@ -54,29 +54,29 @@ private:
     geometry_msgs::msg::Twist msg;
 };
 
-// Vigtig note! i vores "int index" senere bliver det lavet til en integer or dermed afrundet. 
+// Vigtig note! i vores "int index" senere bliver det lavet til en integer or dermed afrundet.
 // Med det antal samples N vi har og vores sample rate bliver
 // 697.5 til index = 696. Det giver den største amplitude ligesom da vi i matlab så at den af en eller anden grund
 // altid var størst ved 696 og ikke 697 som man skulle tro
 
 int lastSound;
-bool shutdown=false;
+bool shutdown = false;
 
-void FFT(const std::vector<double> &audioData, double sampleRate)
+int FFT(const std::vector<double> &audioData, double sampleRate)
 {
     int N = audioData.size();
-    //std::cout << "N = " << N << std::endl;
+    // std::cout << "N = " << N << std::endl;
 
     /// FFTW setup
-    double *in;     //En pointer til en double som senere vil blive til array reelle tal til vores FFT input
-    fftw_complex *out;  //En pointer et komplekst tal, som senere bliver et array til FFT output
+    double *in;        // En pointer til en double som senere vil blive til array reelle tal til vores FFT input
+    fftw_complex *out; // En pointer et komplekst tal, som senere bliver et array til FFT output
     fftw_plan p;
 
     // En 'plan' i FFTW er en forudbestemt strategi for, hvordan man hurtigt kan beregne FFT. Det forbereder den mest effektive måde at transformere data på.
 
-    in = (double *)fftw_malloc(sizeof(double) * N); // Allokere et array af reelle tal af længden N til vores input
+    in = (double *)fftw_malloc(sizeof(double) * N);                        // Allokere et array af reelle tal af længden N til vores input
     out = (fftw_complex *)fftw_malloc(sizeof(fftw_complex) * (N / 2 + 1)); // Ligeledes bare til komplekse output tal
-    
+
     // Kun N/2 + 1 tal fordi vi laver en real to complex FFTW (Fordi der kun reelle tal i lyd og det her er vidst hurtigere)
     // Noget med at noget information er redundant (:)
 
@@ -87,65 +87,131 @@ void FFT(const std::vector<double> &audioData, double sampleRate)
     }
 
     // Her oprettes først planen for vores FFT, derefter udføres den.
-    p = fftw_plan_dft_r2c_1d(N, in, out, FFTW_ESTIMATE); // BEMÆRK! r2c (real to complex). 
-    //FFTW_ESTIMATE betyder at den estimere den hurtigste plan i stedet for at prøve lidt forskelligt af (eller noget)
-    fftw_execute(p); //udfører planen
+    p = fftw_plan_dft_r2c_1d(N, in, out, FFTW_ESTIMATE); // BEMÆRK! r2c (real to complex).
+    // FFTW_ESTIMATE betyder at den estimere den hurtigste plan i stedet for at prøve lidt forskelligt af (eller noget)
+    fftw_execute(p); // udfører planen
 
     double avg;
-    for(int i = 0; i < N/2+1; i++){
+    for (int i = 0; i < N / 2 + 1; i++)
+    {
         avg = avg + std::sqrt(out[i][0] * out[i][0] + out[i][1] * out[i][1]);
     }
 
     // Find største amp på ene led
-    double largestAmp1 = avg/(N/2 +1)*5;
+    double largestAmp1 = avg / (N / 2 + 1) * 5;
     double largestAmp2 = largestAmp1;
     double largestFreq1 = 0;
+    int sound = 0;
 
     for (double i : DTMF1)
-    {        
+    {
         int index = i * (N / sampleRate);
-
         double amp = std::sqrt(out[index][0] * out[index][0] + out[index][1] * out[index][1]);
-        
-        if (amp > largestAmp1){
+
+        if (amp > largestAmp1)
+        {
             largestAmp1 = amp;
             largestFreq1 = i;
         }
     }
-    
+
     // Find største amp på anden led
     double largestFreq2 = 0;
 
-    if(largestFreq1 != 0){
-    for (double i : DTMF2)
+    if (largestFreq1 != 0)
     {
-        int index = i * (N / sampleRate);
+        for (double i : DTMF2)
+        {
+            int index = i * (N / sampleRate);
 
-        double amp = std::sqrt(out[index][0] * out[index][0] + out[index][1] * out[index][1]);
-        
-        if (amp > largestAmp2){
-            largestAmp2 = amp;
-            largestFreq2 = i;
+            double amp = std::sqrt(out[index][0] * out[index][0] + out[index][1] * out[index][1]);
+
+            if (amp > largestAmp2)
+            {
+                largestAmp2 = amp;
+                largestFreq2 = i;
+            }
+        }
+        if (largestFreq2 != 0)
+        {
+            // std::cout << "Largest DTMF1: " << largestFreq1 << " Largest DTMF2: " << largestFreq2 << std::endl;
+
+            sound = largestFreq1 + largestFreq2;
+            if (lastSound == sound)
+            {
+                // De-allokerer hukommelse fra pointers.
+                fftw_destroy_plan(p);
+                fftw_free(in);
+                fftw_free(out);
+                return;
+            }
+
+            lastSound = sound;
+
+            // De-allokerer hukommelse fra pointers.
+            fftw_destroy_plan(p);
+            fftw_free(in);
+            fftw_free(out);
+            return;
         }
     }
-        if(largestFreq2 != 0){
-        //std::cout << "Largest DTMF1: " << largestFreq1 << " Largest DTMF2: " << largestFreq2 << std::endl;
-        
-        int sound = largestFreq1+largestFreq2;
-        if(lastSound == sound){
-            // De-allokerer hukommelse fra pointers. 
-        fftw_destroy_plan(p);
-        fftw_free(in);
-        fftw_free(out);
-        return;
+    // std::cout << "no valid DTMF found" << std::endl;
+    //  De-allokerer hukommelse fra pointers.
+    fftw_destroy_plan(p);
+    fftw_free(in);
+    fftw_free(out);
+
+    return sound;
+}
+
+int main(int argc, char **argv)
+{
+    PaStream *stream;
+    PaError err;
+    int result;
+
+    rclcpp::init(argc, argv);
+    auto rb3_publisher = std::make_shared<RB3_cpp_publisher>();
+    rclcpp::executors::SingleThreadedExecutor executor;
+    executor.add_node(rb3_publisher);
+
+    // Initialize PortAudio
+    err = Pa_Initialize();
+
+    PaStreamParameters inputParameters;
+    inputParameters.device = Pa_GetDefaultInputDevice();
+    inputParameters.channelCount = NUM_CHANNELS;
+    inputParameters.sampleFormat = paFloat32;
+    inputParameters.suggestedLatency = Pa_GetDeviceInfo(inputParameters.device)->defaultHighInputLatency;
+    inputParameters.hostApiSpecificStreamInfo = nullptr;
+
+    err = Pa_OpenStream(&stream, &inputParameters, nullptr, SAMPLE_RATE, FRAMES_PER_BUFFER, paClipOff, nullptr, nullptr);
+
+    err = Pa_StartStream(stream);
+
+    int test = 0;
+
+    while (!shutdown)
+    {
+
+        std::vector<double> audioData;
+        audioData.reserve(SAMPLE_RATE * NUM_CHANNELS * RECORDING_DURATION_SECONDS);
+
+        for (int i = 0; i < RECORDING_DURATION_SECONDS * SAMPLE_RATE / FRAMES_PER_BUFFER; i++)
+        {
+            float buffer[FRAMES_PER_BUFFER];
+            err = Pa_ReadStream(stream, buffer, FRAMES_PER_BUFFER);
+
+            audioData.insert(audioData.end(), buffer, buffer + FRAMES_PER_BUFFER);
         }
-                // Initialize rclcpp
-        switch (sound)
+
+        result = FFT(audioData, SAMPLE_RATE);
+
+        switch (result)
         {
         case 1907:
             std::cout << "1" << std::endl;
-	  rb3_publisher->publish_vel(0.1,0);
-
+            rb3_publisher->publish_vel(0.1, 0);
             break;
         case 2033:
             std::cout << "2" << std::endl;
@@ -154,11 +220,10 @@ void FFT(const std::vector<double> &audioData, double sampleRate)
             break;
         case 2174:
             std::cout << "3" << std::endl;
-	rb3_publisher->publish_vel(0,0.2);
+            rb3_publisher->publish_vel(0, 0.2);
             break;
         case 2330:
             std::cout << "A" << std::endl;
-	 rb3_publisher->publish_vel(0.1, 0);
             break;
         case 1979:
             std::cout << "4" << std::endl;
@@ -195,78 +260,19 @@ void FFT(const std::vector<double> &audioData, double sampleRate)
             break;
         case 2574:
             std::cout << "D" << std::endl;
-        rb3_publisher->publish_vel(0,0);
-	shutdown = true;
- rclcpp::shutdown();
+            shutdown = true;
+            rclcpp::shutdown();
             break;
         default:
             std::cout << "Ewww" << std::endl;
             break;
         }
-        lastSound = sound;
-        
-        // De-allokerer hukommelse fra pointers. 
-        fftw_destroy_plan(p);
-        fftw_free(in);
-        fftw_free(out);
-        return;
-        }
-    } 
-    
-    //std::cout << "no valid DTMF found" << std::endl;
-    // De-allokerer hukommelse fra pointers. 
-    fftw_destroy_plan(p);
-    fftw_free(in);
-    fftw_free(out);
-}
-
-int main(int argc, char **argv)
-{
-    PaStream *stream;
-    PaError err;
-
-    rclcpp::init(argc,argv);
-	auto rb3_publisher = std::make_shared<RB3_cpp_publisher>();
-    rclcpp::executors::SingleThreadedExecutor executor;
-    executor.add_node(rb3_publisher); 
-
-    // Initialize PortAudio
-    err = Pa_Initialize();
-
-    PaStreamParameters inputParameters;
-    inputParameters.device = Pa_GetDefaultInputDevice();
-    inputParameters.channelCount = NUM_CHANNELS;
-    inputParameters.sampleFormat = paFloat32;
-    inputParameters.suggestedLatency = Pa_GetDeviceInfo(inputParameters.device)->defaultHighInputLatency;
-    inputParameters.hostApiSpecificStreamInfo = nullptr;
-
-    err = Pa_OpenStream(&stream, &inputParameters, nullptr, SAMPLE_RATE, FRAMES_PER_BUFFER, paClipOff, nullptr, nullptr);
-
-    err = Pa_StartStream(stream);
-
-    int test = 0;
-
-    while (!shutdown) {
-        
-        std::vector<double> audioData;
-        audioData.reserve(SAMPLE_RATE * NUM_CHANNELS * RECORDING_DURATION_SECONDS);
-
-        for (int i = 0; i < RECORDING_DURATION_SECONDS * SAMPLE_RATE / FRAMES_PER_BUFFER; i++) {
-            float buffer[FRAMES_PER_BUFFER];
-            err = Pa_ReadStream(stream, buffer, FRAMES_PER_BUFFER);
-            
-            audioData.insert(audioData.end(), buffer, buffer + FRAMES_PER_BUFFER);
-        }
-
-        FFT(audioData, SAMPLE_RATE);
-       // std::cout << test;
-        //test++;
     }
 
     err = Pa_StopStream(stream);
 
     err = Pa_CloseStream(stream);
-    
+
     Pa_Terminate();
     return 0;
 }
--- start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Enter IP"
-read ip
-password = "password"
-sshpass -p $password ssh pi@$ip
-
