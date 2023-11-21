b1bc37aa8ca76706012a66509a4ea31ea09b2274
Halakris <lauritsgc@gmail.com>
2023-11-21 13:54:53 +0100

    Infinity Threads Fixed

--- LauritsLyd/playAudio.cpp
+++ LauritsLyd/playAudio.cpp
@@ -142,6 +142,11 @@ void *PlayAudio::audioThread(void *args)
             *selectedKey = '\0'; // Reset selected key after playing
         }
     }
+
+    std::cout << std::this_thread::get_id() << ": Lort" << std::endl;
+
+    pthread_exit(NULL);
+
     mu.unlock();
     return NULL;
 }
