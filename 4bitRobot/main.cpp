2744d140061e8a69c6c9da2a29b6e33695ca05b6
Valdemar Lange <vlange712@gmail.com>
2023-11-21 13:12:25 +0100

    robototototot

--- 4bitRobot/main.cpp
+++ 4bitRobot/main.cpp
@@ -3,10 +3,11 @@
 #include <map>
 #include <utility>
 
+#include "MessageInterpret.h"
 #include "FFT.h"
 #include "PortAudioClass.h"
-#include "rb3_cpp_publisher.h"
-#include "drive.h"
+//#include "rb3_cpp_publisher.h"
+//#include "drive.h"
 #include <unistd.h>
 
 const int sampleRate = 8000;
@@ -16,86 +17,17 @@ const int numChannels = 1;
 
 bool shutdown = false;
 
-std::map<int, std::string> toneToBitMap = {{2277, "000"}, {1907, "001"}, {2033, "010"}, {2174, "011"}, {1979, "100"}, {2106, "101"}, {2247, "110"}, {2061, "111"}, {2188, "0"}, {2329, "1"}};
 
-int lastSequenceNumber = 0;
-
-std::vector<std::pair<int, std::string>> driveCommands;
-
-void interpretMessage(std::vector<int> &inputSekvens, Drive *robo)
-{
-    //Create string of bits
-    std::string bits;
-    for (int i = 0; i < inputSekvens.size(); i++)
-    {
-        bits = bits + toneToBitMap[inputSekvens[i]];
-    }
-    std::cout << bits << std::endl;
-
-    //Parity Check:
-    int oneCount = 0;
-    for (char c : bits)
-    {
-        if (c == '1'){
-            oneCount++;
-        }       
-    }
-    std::cout << oneCount << std::endl;
-    if (bits.substr(bits.length() - 1) == "1" && !oneCount % 2 == 0)
-    {
-        std::cout << "parity fail" << std::endl;
-        //return error?;
-    }
-    
-        // Sequence number check
-
-    int sequenceNumber = stoi(bits.substr(0, 6), nullptr, 2);
-
-    std::cout << "seqNr: " << sequenceNumber << std::endl;
-
-    if (sequenceNumber != lastSequenceNumber + 1)
-    {
-        lastSequenceNumber = 0; // eller hvad skal der ske ved fejl
-        std::cout << "sequence number fail" << std::endl;
-        //return error?;
-    } 
-    lastSequenceNumber = sequenceNumber;
-
-        //Execute command
-    int commandInt = stoi(bits.substr(6, 6), nullptr, 2);
-    
-    int dataFieldBitLength = bits.size() - 13; //13bits = 6 til seqNr., 6 til command og 1 til parity
-    std::string data = bits.substr(12, dataFieldBitLength);
-
-    switch (commandInt) 
-    {
-    case 0b111110: //Command-code
-        driveCommands.push_back(std::make_pair(commandInt, data));
-        break;
-    case 0b101010:
-        driveCommands.push_back(std::make_pair(commandInt, data));
-        break;
-    case 0b010111: //Command-code
-        driveCommands.push_back(std::make_pair(commandInt, data));
-        break;
-    case 0b101110:
-        driveCommands.push_back(std::make_pair(commandInt, data));
-        break;
-    default:
-        break;
-    }
-
-}
 
 int main(int argc, char **argv)
 {
-
+    /*
     rclcpp::init(argc, argv);
     auto rb3_publisher = std::make_shared<RB3_cpp_publisher>();
     rclcpp::executors::SingleThreadedExecutor executor;
     executor.add_node(rb3_publisher);
 
-    Drive robo(rb3_publisher);
+    Drive robo(rb3_publisher); */
 
     int result;
     DTMFDecoder decoder(1600);
@@ -157,9 +89,6 @@ int main(int argc, char **argv)
                 shutdown = true;
             }
         }
-        // auto end = std::chrono::high_resolution_clock::now();
-        // std::chrono::duration<double, std::milli> duration = end - start;
-        // std::cout << "Processing cycle took " << duration.count() << " milliseconds.\n";
     }
     rclcpp::shutdown();
     return 0;
