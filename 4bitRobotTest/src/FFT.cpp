ab8a2b696cf6d8588192bf6069b166a719181ed5
Valdemar Lange <vlange712@gmail.com>
2023-12-11 09:20:57 +0100

    pislort

--- 4bitRobotTest/src/FFT.cpp
+++ 4bitRobotTest/src/FFT.cpp
@@ -20,69 +20,6 @@ DTMFDecoder::~DTMFDecoder()
     fftw_destroy_plan(plan);
 }
 
-// Implement the calculateMedian function
-double DTMFDecoder::calculateMedian(const std::vector<float> &vec)
-{
-    if (vec.empty())
-    {
-        std::cerr << "Error: Cannot calculate median of an empty vector." << std::endl;
-        return 0.0;
-    }
-
-    // Sort the vector
-    std::vector<float> sortedVec = vec;
-    std::sort(sortedVec.begin(), sortedVec.end());
-
-    // Calculate the median
-    size_t size = sortedVec.size();
-    size_t middle = size / 2;
-
-    if (size % 2 == 0)
-    {
-        // If the size is even, take the average of the two middle elements
-        return static_cast<double>(sortedVec[middle - 1] + sortedVec[middle]) / 2.0;
-    }
-    else
-    {
-        // If the size is odd, return the middle element
-        return static_cast<double>(sortedVec[middle]);
-    }
-}
-
-double DTMFDecoder::calculateAverageOfLast10Medians(const std::vector<float> &audioData)
-{
-    // Calculate the current median
-    double currentMedian = calculateMedian(audioData);
-
-    // Determine the current sound level (maximum value in audioData)
-    float maxSoundLevel = *std::max_element(audioData.begin(), audioData.end());
-
-    // Update the currentValue based on the sound level
-    double currentValue = maxSoundLevel; // You can apply any scaling or transformation here
-
-    // Add the current median to the list of last 10 medians
-    this->last10Medians.push_back(currentMedian);
-
-    // Keep only the last 5 medians
-    if (this->last10Medians.size() > 5)
-    {
-        this->last10Medians.erase(this->last10Medians.begin());
-    }
-
-    // Calculate the average of the last 10 medians
-    double sum = 0.0;
-    for (double median : this->last10Medians)
-    {
-        sum += median;
-    }
-
-    // Calculate the average including the current value and a baseline
-    double baseline = 2.0;                                                               // Replace this with your desired baseline value
-    double average = (sum + currentValue + baseline) / (this->last10Medians.size() + 2); // 2 to account for currentValue and baseline
-
-    return average;
-}
-
 void DTMFDecoder::setStartBit(bool in)
 {
     startBit = in;
@@ -110,18 +47,22 @@ int DTMFDecoder::FFT(const std::vector<float> &audioData, double sampleRate)
 
     // Execute the FFT plan
     fftw_execute(plan);
+    
     double threshold = 2;
+    double tolerance = 25;
+
     // double threshold = abs(calculateAverageOfLast10Medians(audioData)*100); // LAV NOGET FEDT TIL THRESHOLD
     double largestAmp1 = threshold;
     double largestAmp2 = threshold;
     double largestFreq1 = 0.0;
     double largestFreq2 = 0.0;
     int detectedSound = 0;
+    
 
     // Function to check if a frequency is within the range of a target frequency
-    auto isFrequencyInRange = [](double target, double actual, double tolerance)
+    auto isFrequencyInRange = [](double target, double actual, double _tolerance)
     {
-        return std::abs(target - actual) <= tolerance;
+        return std::abs(target - actual) <= _tolerance;
     };
 
     // Adjusted detection logic
@@ -132,7 +73,7 @@ int DTMFDecoder::FFT(const std::vector<float> &audioData, double sampleRate)
 
         for (double targetFreq : DTMF1)
         {
-            if (isFrequencyInRange(targetFreq, freq, 25.0) && amp > largestAmp1)
+            if (isFrequencyInRange(targetFreq, freq, tolerance) && amp > largestAmp1)
             {
                 largestAmp1 = amp;
                 largestFreq1 = targetFreq; // Use the target frequency, not the actual
@@ -141,7 +82,7 @@ int DTMFDecoder::FFT(const std::vector<float> &audioData, double sampleRate)
 
         for (double targetFreq : DTMF2)
         {
-            if (isFrequencyInRange(targetFreq, freq, 25.0) && amp > largestAmp2)
+            if (isFrequencyInRange(targetFreq, freq, tolerance) && amp > largestAmp2)
             {
                 largestAmp2 = amp;
                 largestFreq2 = targetFreq; // Use the target frequency, not the actual
